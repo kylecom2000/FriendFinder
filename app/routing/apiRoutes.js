3820b5fd41cd07e55a943f0bcd2429b76740304d
FightingSprrow <kbauertx@gmail.com>
2018-12-04 17:20:53 -0600

    returns array of differences and closest difference number

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,72 +1,15 @@
-// 4. Your `apiRoutes.js` file should contain two routes:
-
-//    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
-//    * A POST routes `/api/friends`. 
-//          This will be used to handle incoming survey results. 
-//          This route will also be used to handle the compatibility logic.
-
 const friends = require("../data/friends");
 const express = require("express");
 const app = express();
-
 
 app.get('/friends', function(req, res){
     res.send(friends);
 })
 
-app.route('/')
-    .get(function (req, res) {
-        res.send(friends);
-    }) // END OF GET
-
-
-    .post(function (req, res) {
-        
-
-        // return (res);
-        // for (let i = 0; i < friends.length; i++){
-        //     if (friends[0].score[0] === 1){
-        //         console.log
-        //     }
-        //     //Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
-            
-        // }
-        // The return on this function goes to the data for the $("#match-name") and $("#match-img")
-
-
-        friends.push(req.body);
-        res.send(friends);
-    }) // END OF POST
-
-
-
-    .put(function (req, res) {
-        console.log('Update a friend')
-    }) // END OF PUT
-
-
-module.exports = app;
-
-
-
-
-
-
-// Input  : Math.abs(-4)
-// Output : 4
-
-
-
-// https://www.diffen.com/difference/GET-vs-POST-HTTP-Requests
-
-// https://expressjs.com/en/guide/routing.html
-
-
-
-
-
+//    * A POST routes `/api/friends`. 
+//          This will be used to handle incoming survey results. 
+//          This route will also be used to handle the compatibility logic.
 // 6. Determine the user's most compatible friend using the following as a guide:
-
 
 //    * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
 //      * Example:
@@ -78,3 +21,58 @@ module.exports = app;
 
 // 7. Once you've found the current user's most compatible friend, display the result as a modal pop-up.
 //    * The modal should display both the name and picture of the closest match.
+
+app.post('/friends', function (req, res) {
+
+    // push new friend into the friends array at the beginning [0]
+    friends.unshift(req.body); 
+
+    // assigning webpage input scores into a variable
+    let newFriendScores = req.body.scores;
+
+    // an array to hold the differences of each friend's score between each friends array.
+    let allDifferencesArray = [];
+
+    // loop for each difference score.
+    for (let i = 0; i < friends.length; i++){
+
+        // Variable to hold each score's difference.
+        let differenceScore = 0;
+
+        // loop for each score within each scores array of each friend.
+        for (let j = 0; j < friends[i].scores.length; j++){
+            differenceScore += Math.abs(friends[i].scores[j]-newFriendScores[j]);
+        }
+        // push each difference score to the end of allDifferencesArray.
+        allDifferencesArray.push(differenceScore);
+    }
+    let currentMatch = "";
+    console.log(allDifferencesArray);
+    function closestFriend(arr){
+        var curr = arr[0];
+        var diff = Math.abs (0 - curr);
+        for (var val = 0; val < arr.length; val++) {
+            var newdiff = Math.abs (0 - arr[val]);
+            if (newdiff < diff) {
+                diff = newdiff;
+                curr = arr[val];
+            }
+        }
+        return curr;
+    }
+    
+
+    console.log(closestFriend(allDifferencesArray.slice(1)));
+    res.send(friends);
+}) 
+
+
+ // The return on this function goes to the data for the $("#match-name") and $("#match-img")
+module.exports = app;
+
+// Input  : Math.abs(-4)
+// Output : 4
+
+// https://www.diffen.com/difference/GET-vs-POST-HTTP-Requests
+// https://expressjs.com/en/guide/routing.html
+
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,26 +1,23 @@
-// 3. Your `htmlRoutes.js` file should include two routes: -KMB
-// * A GET Route to `/survey` which should display the survey page. -KMB
-// * A default, catch-all route that leads to `home.html` which displays the home page. -KMB
-
-
 const express = require("express");
 const path = require("path");
 const app = express();
 
+// Root html
 app.get("/", function(req,res) {
     console.log("/root route...");
     res.sendFile(path.join(__dirname, "../public/home.html"));
 });
 
+// Survey html
 app.get("/survey", function(req, res){
     console.log("/Survey route...");
     res.sendFile(path.join(__dirname, "../public/survey.html"));
 })
 
+// Catch all
 app.get("*", function(req,res) {
     console.log("/other route...");
     res.sendFile(path.join(__dirname, "../public/home.html"));
 });
-
 
 module.exports = app;

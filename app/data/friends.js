9531de62d707a26316d2bc2f14c2ae3e5a566a26
FightingSprrow <kbauertx@gmail.com>
2018-12-03 21:32:47 -0600

    html form pushes to friends.js array

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,69 +7,162 @@ let friendsArrayofObjects =[
   "name":"Kyle B.",
   "photo":"https://i.kinja-img.com/gawker-media/image/upload/s--Tg_qqR3r--/c_scale,f_auto,fl_progressive,q_80,w_800/dnmtn4ksijwyep0xmljk.jpg",
   "scores":[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
     ]
 },
 {
   "name":"Different C.",
   "photo":"https://pbs.twimg.com/profile_images/1717956431/BP-headshot-fb-profile-photo_400x400.jpg",
   "scores":[
-      5,
-      2,
-      1,
-      2,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
+      "5",
+      "2",
+      "1",
+      "2",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
     ]
 },
 {
-  "name":"Different D.",
-  "photo":"https://pbs.twimg.com/profile_images/1717956431/BP-headshot-fb-profile-photo_400x400.jpg",
+  "name":"Satan",
+  "photo":"https://pbs.twimg.com/profile_images/738139405446062081/x0FQk9Yl_400x400.jpg",
   "scores":[
-      1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      1
+      "5",
+      "5",
+      "5",
+      "4",
+      "5",
+      "4",
+      "2",
+      "4",
+      "3",
+      "1"
+    ]
+},
+{
+  "name":"Jesus",
+  "photo":"http://3.bp.blogspot.com/-LMQ6hkLNLUo/UTLSujDGaII/AAAAAAAAB94/ZS8E6Mnt0ks/s1600/jesus_thumbs-up.jpg",
+  "scores":[
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1"
+    ]
+},
+{
+  "name":"Carl Sagan",
+  "photo":"https://i2.wp.com/www.brainpickings.org/wp-content/uploads/2013/11/carlsagan1.jpg?w=680&ssl=1",
+  "scores":[
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3"
+    ]
+},
+{
+  "name":"Bill Nye",
+  "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Bill_Nye_by_Gage_Skidmore.jpg/170px-Bill_Nye_by_Gage_Skidmore.jpg",
+  "scores":[
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5"
+    ]
+},
+{
+  "name":"The Beebs",
+  "photo":"https://peopledotcom.files.wordpress.com/2018/10/justin-bieber.jpg?crop=0px%2C0px%2C1000px%2C525px&resize=1200%2C630",
+  "scores":[
+      "2",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2"
+    ]
+},
+{
+  "name":"The Rock",
+  "photo":"https://vice-images.vice.com/images/content-images/2015/10/16/if-you-smell-what-the-rock-is-cooking-101-body-image-1444992411.jpg?output-quality=75?resize=320:*",
+  "scores":[
+      "3",
+      "1",
+      "3",
+      "1",
+      "3",
+      "1",
+      "3",
+      "1",
+      "3",
+      "1"
+    ]
+},
+{
+  "name":"Carl Sagan",
+  "photo":"https://i2.wp.com/www.brainpickings.org/wp-content/uploads/2013/11/carlsagan1.jpg?w=680&ssl=1",
+  "scores":[
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3"
+    ]
+},
+{
+  "name":"Your Mom",
+  "photo":"https://pbs.twimg.com/profile_images/827125890563862529/w85oJp27_400x400.jpg",
+  "scores":[
+      "2",
+      "3",
+      "2",
+      "3",
+      "2",
+      "3",
+      "2",
+      "3",
+      "2",
+      "3"
     ]
 }];
 
-
-// 6. Determine the user's most compatible friend using the following as a guide:
-
-//    * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
-//    * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
-//      * Example:
-//        * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
-//        * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
-//        * Total Difference: **2 + 1 + 2 =** **_5_**
-//    * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on.
-//    * The closest match will be the user with the least amount of difference.
-
-// 7. Once you've found the current user's most compatible friend, display the result as a modal pop-up.
-//    * The modal should display both the name and picture of the closest match.
-
-
-
 // http://www.tutorialsteacher.com/nodejs/nodejs-module-exports
-
-
 module.exports = friendsArrayofObjects;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,30 +15,66 @@ app.get('/friends', function(req, res){
 })
 
 app.route('/')
-  .get(function (req, res) {
-    console.log('GET request to the homepage');
-    console.log(friends);
-    res.send(friends);
-  })
-  .post(function (req, res) {
-    console.log(req.body);
-    res.send('POST request to the homepage');
-  })
-  .put(function (req, res) {
-    console.log('Update a friend')
-  })
+    .get(function (req, res) {
+        res.send(friends);
+    }) // END OF GET
 
+
+    .post(function (req, res) {
+        
+
+        // return (res);
+        // for (let i = 0; i < friends.length; i++){
+        //     if (friends[0].score[0] === 1){
+        //         console.log
+        //     }
+        //     //Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
+            
+        // }
+        // The return on this function goes to the data for the $("#match-name") and $("#match-img")
+
+
+        friends.push(req.body);
+        res.send(friends);
+    }) // END OF POST
+
+
+
+    .put(function (req, res) {
+        console.log('Update a friend')
+    }) // END OF PUT
 
 
 module.exports = app;
+
+
+
+
+
+
+// Input  : Math.abs(-4)
+// Output : 4
+
 
 
 // https://www.diffen.com/difference/GET-vs-POST-HTTP-Requests
 
 // https://expressjs.com/en/guide/routing.html
 
-// exports ROUTES
-// exports CompatabilityLogic?
 
 
 
+
+// 6. Determine the user's most compatible friend using the following as a guide:
+
+
+//    * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
+//      * Example:
+//        * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
+//        * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
+//        * Total Difference: **2 + 1 + 2 =** **_5_**
+//    * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on.
+//    * The closest match will be the user with the least amount of difference.
+
+// 7. Once you've found the current user's most compatible friend, display the result as a modal pop-up.
+//    * The modal should display both the name and picture of the closest match.
